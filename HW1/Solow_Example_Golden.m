94dc04723296572297b61309caa7c0929119c872
Matt <mchistolini@colgate.edu>
2021-02-03 21:35:18 -0500

    adding stuff

---
+++ HW1/Solow_Example_Golden.m
@@ -0,0 +1,25 @@
+clear;clc;
+
+
+kgolden=(1.5/.12)^(1/.6);
+LLL = (1.5/.12);
+pow = .6;
+sig=.26*kgolden/(5*kgolden^0.2);
+
+sigval=zeros(101,1);
+cssval=zeros(101,1);
+tech = 10;
+for jj=1:101
+    val=(jj-1)/100;
+    sigval(jj)=val;
+    kss=(tech*val/LLL)^pow;
+    cssval(jj)=(1-val)*tech*kss^(1-pow);
+end
+
+plot(cssval)
+
+[a,b]=max(cssval);
+'maximum steady state consumption per worker';
+a
+'saving rate to maximize steady state consumption per worker'
+sigval(b)
