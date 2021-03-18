655c0a7dd7c1c57a2c64f9b2d81e52b6f783b9d7
Matt <mchistolini@colgate.edu>
2021-03-17 22:52:07 -0400

    finished Mlabgit add .

--- HW3/function_for_q2.m
+++ HW3/function_for_q2.m
@@ -1,10 +1,10 @@
-function val=valfun(k)
-global vlast BETTA ALFA k0 kt theta delta sigma
+function val=function_for_q2(k)
+global vlast BETTA k0 kt theta delta sigma
 %smooth out the previous value function
 g=interp1(k0,vlast,k,'linear');
 %Calculate consumption with given paraameters
 % I am unsruee if k= kt+1 or k =kt
-c= kt-(1-delta)*k - k^theta  ;
+c=kt^theta -k + kt*(1-delta);
 if c <= 0
     %to keep values from going negative
     val=-888-800*abs(c);
--- HW3/question_2_e.m
+++ HW3/question_2_e.m
@@ -1,8 +1,9 @@
 clear;clc;clf;
-global vlast BETTA k0 kt theta delta sigma
+global vlast BETTA k0 kt theta delta sigma EE
 
 
 %Calibration
+EE = 20;
 BETTA=0.95;
 theta = 0.3;
 delta = 0.025;
@@ -33,11 +34,11 @@ while jk<=maxits
     for j=1:numgrid
         kt=k0(j);
         %find the maximum of the value function (minus the minimum)
-        ktp1=fminbnd(@function_for_q2,0,maxk);
+        ktp1=fminbnd(@function_for_q2_e,0,maxk);
         %Note: valfun calculates the negative of the value function since
         %fminbnd finds a minimum
         kt_plus_one_vector(j)=ktp1;
-        v(j)=-function_for_q2(ktp1);
+        v(j)=-function_for_q2_e(ktp1);
     end
     
 %if norm of difference vector is less than tolerance level, stop
@@ -60,4 +61,4 @@ plot(k0,k0)
 hold on
 plot(k0,kt_plus_one_vector)
 legend('straight line','K_{t+1}')
-saveas(gcf,'pics/HW3_Q2_figure.png')
+saveas(gcf,'pics/HW3_Q2_figure_e.png')
